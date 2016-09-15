4437412ececf9db5f807aa084bd82943a616833b
Shaika-Dzari <shaikadzari@4nakama.net>
2016-09-15 08:23:31 -0400

    Add a schema

--- imports/api/blogpost.js
+++ imports/api/blogpost.js
@@ -5,10 +5,30 @@ import { check } from 'meteor/check';
 
 export const BlogPosts = new Mongo.Collection('blogpost');
 
+BlogPosts.schema = new SimpleSchema({
+    title: { type: String },
+    text: { type: String },
+    published: { type: Number, defaultValue: 0 },
+    createdAt: { type: Date, defaultValue: new Date() },
+    updatedAt: { type: Date },
+    authorid: { type: Number },
+    authorname: { type: String },
+    permalink: { type: String },
+    categories: { type: [String] },
+    tags: { type: [String], optional: true }
+});
+
+if (Meteor.isServer) {
+    Meteor.publish('blogpost', function blogPostPublication() {
+        return Tasks.find();
+    });
+}
+
 Meteor.methods({
     'blogpost.insert'(title, text) {
         check(text, String);
         check(title, String);
+
         // Make sure the user is logged in before inserting a task
         if (!this.userId) {
             throw new Meteor.Error('not-authorized');
